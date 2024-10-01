469e856232d3a34279613e46fe738d0b1f046fcb
Chetan <chetanmahakalkar5010@gmail.com>
2024-10-01 19:09:32 +0530

    Changes added

--- src/main/java/com/cybage/tripconnect/HotelController.java
+++ src/main/java/com/cybage/tripconnect/HotelController.java
@@ -2,6 +2,9 @@ package com.cybage.tripconnect;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HotelController {
@@ -10,5 +13,10 @@ public class HotelController {
     @GetMapping("/hotel")
     public String getData() {
         return "Please book hotel from tripConnect with 75% discount in Maharashtra";
+    }
+
+    @PutMapping("/hotel/{hotelId}")
+    public String updateHotel(@PathVariable String hotelId, @RequestParam String status) {
+        return "Hotel with ID: " + hotelId + " is now " + status;
     }
 }
